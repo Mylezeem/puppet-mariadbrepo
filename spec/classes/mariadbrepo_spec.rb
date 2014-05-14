cb275dbfb85551b33afaa3f70158ff722798eb71
Yanis Guenane <yguenane@gmail.com>
2014-05-13 22:41:26 -0400

    Adapt unit test to use yumrepo instead of template file

--- spec/classes/mariadbrepo_spec.rb
+++ spec/classes/mariadbrepo_spec.rb
@@ -5,15 +5,53 @@ describe 'mariadbrepo' do
   ['Fedora', 'RedHat', 'CentOS'].each do |operatingsystem|
     ['5.5', '10.0'].each do |mariadb_release|
       ['i386', 'x86_64'].each do |architecture|
+
         context "Operating System #{operatingsystem} / MariaDB Release #{mariadb_release} / Architecture #{architecture}" do
+
+          case operatingsystem
+          when 'Fedora'
+            os        = 'fedora'
+            vers_maj  = '20'
+            vers_full = '20'
+          when 'RedHat'
+            os        = 'rhel'
+            vers_maj  = '6'
+            vers_full = '6.5'
+          when 'CentOS'
+            os   = 'centos'
+            vers_maj  = '6'
+            vers_full = '6.5'
+          end
+
+          case architecture
+          when 'i386'
+            arch = 'x86'
+          when 'x86_64'
+            arch = 'amd64'
+          end
+
           let(:facts) do
-            { :operatingsystem => operatingsystem, :architecture => architecture }
+            {
+              :operatingsystem        => operatingsystem,
+              :operatingsystemrelease => vers_full,
+              :architecture           => architecture,
+            }
           end
+
           let(:params) do
-            {'version' => mariadb_release,
-             'repo_desc_path' => '/etc/yum.repos.d'}
+            {
+              :version => mariadb_release,
+            }
           end
-          it { should contain_file('/etc/yum.repos.d/mariadb.repo') }
+
+          it {
+            should contain_yumrepo('MariaDB').with({
+              'baseurl'  => "http://yum.mariadb.org/#{mariadb_release}/#{os}#{vers_maj}-#{arch}",
+              'gpgkey'   => 'https://yum.mariadb.org/RPM-GPG-KEY-MariaDB',
+              'gpgcheck' => 'true',
+            })
+          }
+
         end
       end
     end
